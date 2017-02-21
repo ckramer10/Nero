eabc033c5d50f6305843590ce9d7faa1ed662e02
pdorsch6 <pdorsch6@gatech.edu>
2017-02-20 22:14:52 -0500

    queries

--- app/src/main/java/com/sffilps/waterlocater/HomeScreen.java
+++ app/src/main/java/com/sffilps/waterlocater/HomeScreen.java
@@ -13,6 +13,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by ckramer on 2/10/17.
@@ -22,9 +28,11 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
 
     private Button signoutButton;
     private FirebaseAuth mAuth;
-    private TextView email;
+    private TextView name;
     FirebaseUser currentUser;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    DatabaseReference mDatabase;
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +40,34 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_login_home);
 
         signoutButton = (Button) findViewById(R.id.signout_button);
-        email = (TextView) findViewById(R.id.email_text);
+        name = (TextView) findViewById(R.id.email_text);
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
         signoutButton.setOnClickListener(this);
 
         if (currentUser != null) {
-            String emailAdd = currentUser.getEmail();
-            email.setText("Welcome, " + emailAdd);
+            String email = currentUser.getEmail();
+            final String uID = currentUser.getUid();
+            mDatabase.child(uID).addListenerForSingleValueEvent(
+                    new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // Get user value
+                            setUserName((String)dataSnapshot.child("name").getValue());
+                            name.setText("Name: " + userName);
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
         } else {
-            email.setText("Didn't work");
+            name.setText("Didn't work");
         }
-
-
     }
+
 
 
     @Override
@@ -57,5 +79,9 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
             context.startActivity(intent);
             Toast.makeText(HomeScreen.this, "Signed Out.",Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void setUserName(String s) {
+        userName = s;
     }
 }
