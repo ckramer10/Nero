3d0129d40d673e1ffa06a281949b8611d9331980
Carl <carl.mubarak@me.com>
2017-03-06 21:11:16 -0500

    compile files

---
+++ app/src/main/java/com/sffilps/waterlocater/model/Person.java
@@ -0,0 +1,20 @@
+package com.sffilps.waterlocater.model;
+
+/**
+ * Created by Carl on 3/6/17.
+ */
+
+public class Person {
+
+    public String name;
+    public String email;
+    public String role;
+    public String uID;
+
+    public Person(String name, String email, String role, String uID) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.uID = uID;
+    }
+}
