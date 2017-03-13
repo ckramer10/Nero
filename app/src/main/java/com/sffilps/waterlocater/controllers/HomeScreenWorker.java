3f61229410a0c8ec0780a9b326a124c2ca054b8f
Case Kramer <ckramer@ipsec-143-215-17-29.vpn.gatech.edu>
2017-03-12 20:37:02 -0400

    Created Quality Report Interface

---
+++ app/src/main/java/com/sffilps/waterlocater/controllers/HomeScreenWorker.java
@@ -0,0 +1,157 @@
+package com.sffilps.waterlocater.controllers;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.sffilps.waterlocater.R;
+
+/**
+ * Created by ckramer on 3/12/17.
+ */
+
+public class HomeScreenWorker extends AppCompatActivity {
+    private Button signoutButton;
+    private Button submitReport;
+    private Button viewReportsList;
+    private Button viewReportsMap;
+    private Button viewQualityReports;
+    private FirebaseAuth mAuth;
+    private TextView name;
+    FirebaseUser currentUser;
+    DatabaseReference mDatabase;
+    private String userName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_home_worker);
+
+        signoutButton = (Button) findViewById(R.id.signout_button);
+        submitReport = (Button) findViewById(R.id.submitreport);
+        viewReportsList = (Button) findViewById(R.id.viewwsourceslist);
+        viewReportsMap = (Button) findViewById(R.id.viewwsourcesmap);
+        viewQualityReports = (Button) findViewById(R.id.viewQualityReports);
+        name = (TextView) findViewById(R.id.email_text);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.getInstance().signOut();
+                Context context = v.getContext();
+                Intent intent = new Intent(context, SplashScreen.class);
+                context.startActivity(intent);
+                Toast.makeText(HomeScreenWorker.this, "Signed Out.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        submitReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, SubmitReport.class);
+                context.startActivity(intent);
+            }
+        });
+
+        viewReportsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, ReportListView.class);
+                context.startActivity(intent);
+            }
+        });
+
+        viewReportsMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, ReportMapView.class);
+                context.startActivity(intent);
+            }
+        });
+
+        viewQualityReports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, QualityReportHome.class);
+                context.startActivity(intent);
+            }
+        });
+
+        if (currentUser != null) {
+            String email = currentUser.getEmail();
+            final String uID = currentUser.getUid();
+            mDatabase.child(uID).addListenerForSingleValueEvent(
+                    new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // Get user value
+                            setUserName((String) dataSnapshot.child("name").getValue());
+                            name.setText("Welcome, " + userName);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+        } else {
+            name.setText("Didn't work");
+        }
+
+    }
+
+
+    /**
+     * A method to set the user name
+     */
+    public void setUserName(String s) {
+        userName = s;
+    }
+
+
+
+    public void onBackPressed(){
+        final DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        Context context = getApplicationContext();
+                        Intent intent = new Intent(context, LoginScreen.class);
+                        context.startActivity(intent);
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to sign out?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+    }
+}
